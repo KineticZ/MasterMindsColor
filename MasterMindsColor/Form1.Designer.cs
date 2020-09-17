00466e9f443f6cc8b27aa0017961ecd12c9cbc47
Zanlin Oo <zanlin87@gmail.com>
2020-09-17 10:09:25 -0700

    Add project files.

---
+++ MasterMindsColor/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace MasterMindsColor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Hint_Button = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(65, 109);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(65, 57);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(103, 46);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(332, 57);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(103, 46);
+            this.panel2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(332, 109);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 44);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(584, 57);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(103, 46);
+            this.panel3.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(584, 109);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(103, 44);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(332, 309);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(103, 46);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "selectorButton";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(65, 234);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(103, 46);
+            this.panel4.TabIndex = 2;
+            // 
+            // panel5
+            // 
+            this.panel5.Location = new System.Drawing.Point(332, 234);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(103, 46);
+            this.panel5.TabIndex = 2;
+            // 
+            // panel6
+            // 
+            this.panel6.Location = new System.Drawing.Point(584, 234);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(103, 46);
+            this.panel6.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Hint_Button
+            // 
+            this.Hint_Button.Location = new System.Drawing.Point(584, 332);
+            this.Hint_Button.Name = "Hint_Button";
+            this.Hint_Button.Size = new System.Drawing.Size(75, 23);
+            this.Hint_Button.TabIndex = 7;
+            this.Hint_Button.Text = "Hint";
+            this.Hint_Button.UseVisualStyleBackColor = true;
+            this.Hint_Button.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Hint_Button_MouseDown);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Hint_Button);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button Hint_Button;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ MasterMindsColor/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace MasterMindsColor
+{
+    public partial class Form1 : Form
+    {
+        private Random random;
+        private Color[] colors;
+        private Dictionary<Button, Panel> keyValuePairs;
+        private Panel[] playerPanels;
+        private Panel[] guessingPanels;
+        public Form1()
+        {
+            InitializeComponent();
+            colors = new Color[] { Color.Red, Color.Green, Color.Blue };
+
+            random = new Random();
+
+            playerPanels = new Panel[] { panel1, panel2, panel3 };
+            guessingPanels = new Panel[] { panel4, panel5, panel6 };
+
+            keyValuePairs = new Dictionary<Button, Panel>();
+            keyValuePairs.Add(button1, panel1);
+            keyValuePairs.Add(button2, panel2);
+            keyValuePairs.Add(button3, panel3);
+
+            panel4.Visible = false;
+            panel5.Visible = false;
+            panel6.Visible = false;
+
+
+            for (int i = 0; i < guessingPanels.Length; i++)
+            {
+                guessingPanels[i].BackColor = colors[i];
+            }
+
+            colorDialog1.AllowFullOpen = false;
+            colorDialog1.AnyColor = false;
+        }
+
+        private void GenerateColorArray()
+        {
+            timer1.Enabled = false;
+            for (int i = 0; i < guessingPanels.Length; i++)
+            {
+                colors[i] = Color.FromArgb(random.Next(2) * 255, random.Next(2) * 255, random.Next(2) * 255);
+                guessingPanels[i].BackColor = colors[i];
+                guessingPanels[i].Visible = false;
+            }            
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            var s = sender as Button;
+            colorDialog1.ShowDialog();
+            keyValuePairs[s].BackColor = colorDialog1.Color;
+            //var s = sender as Button;
+            //keyValuePairs[s] = keyValuePairs[s] >= colors.Length ? 0 : keyValuePairs[s];
+            //panel1.BackColor = colors[keyValuePairs[s]];
+            //keyValuePairs[s]++;
+        }
+        
+        private bool CheckMatchingColors(Panel panel, Panel panelToCheck)
+        {
+            if (panel.BackColor == panelToCheck.BackColor)
+            {
+                panelToCheck.Visible = true;
+                return true;
+            }
+            return false;
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Check if the colors match
+            //Dislay the color panels that match
+            bool isPanel1Matched = CheckMatchingColors(panel1, panel4);
+            bool isPanel2Matched = CheckMatchingColors(panel2, panel5);
+            bool isPanel3Matched = CheckMatchingColors(panel3, panel6);
+
+            if (isPanel1Matched && isPanel2Matched && isPanel3Matched)
+            {
+                timer1.Enabled = true;
+                timer1.Interval = 2000;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+            => GenerateColorArray();
+
+        private void Hint_Button_MouseDown(object sender, MouseEventArgs e)
+        {
+            timer2.Enabled = true;
+            timer2.Interval = 1000;
+
+            for (int i = 0; i < guessingPanels.Length; i++)
+            {
+                guessingPanels[i].Visible = true;
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            timer2.Enabled = false;
+            for (int i = 0; i < guessingPanels.Length; i++)
+            {
+                guessingPanels[i].Visible = false;
+            }
+        }
+    }
+}
